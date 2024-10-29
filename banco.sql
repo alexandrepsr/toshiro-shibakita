4944b48fea38506d29fd032cc440103411f2292b
alexadrepaesrosa@gmail.com <alexandrepaesrosa@gmail.com>
2024-10-29 10:02:18 -0300

    change table

--- banco.sql
+++ banco.sql
@@ -1,8 +1,10 @@
-CREATE TABLE dados (
-    AlunoID int,
-    Nome varchar(50),
-    Sobrenome varchar(50),
-    Endereco varchar(150),
-    Cidade varchar(50),
-    Host varchar(50)
+CREATE TABLE produtos (
+    id_produto INT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT,
+    preco DECIMAL(10,2) NOT NULL,
+    quantidade_estoque INT NOT NULL,
+    categoria VARCHAR(50),
+    imagem VARCHAR(255),
+    marca VARCHAR(50)
 );
