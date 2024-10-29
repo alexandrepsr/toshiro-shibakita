ccb9f20efc8fe9a74b2e7f14b3542ea70230aeab
alexadrepaesrosa@gmail.com <alexandrepaesrosa@gmail.com>
2024-10-29 10:02:41 -0300

    adjust application

--- index.php
+++ index.php
@@ -1,48 +1,39 @@
 <html>
 
 <head>
-<title>Exemplo PHP</title>
+  <title>Exemplo PHP</title>
 </head>
+
 <body>
 
-<?php
-ini_set("display_errors", 1);
-header('Content-Type: text/html; charset=iso-8859-1');
+  <?php
+  ini_set("display_errors", 1);
+  header('Content-Type: text/html; charset=iso-8859-1');
 
+  echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
+  $servername = "54.134.121.21";
+  $username = "root";
+  $password = "password";
+  $database = "teste";
 
-echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
+  $link = new mysqli($servername, $username, $password, $database);
 
-$servername = "54.234.153.24";
-$username = "root";
-$password = "Senha123";
-$database = "meubanco";
-
-// Criar conexão
-
-
-$link = new mysqli($servername, $username, $password, $database);
-
-/* check connection */
-if (mysqli_connect_errno()) {
+  /* check connection */
+  if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
-}
+  }
 
-$valor_rand1 =  rand(1, 999);
-$valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
-$host_name = gethostname();
+  $query = "INSERT INTO produtos (nome, descricao, preco, quantidade_estoque, categoria, marca) VALUES ('Galaxy S23', 'Smartphone top de linha com tela dinâmica AMOLED', 5999.99, 100, 'Eletrônicos', 'Samsung');";
 
+  if ($link->query($query) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $link->error;
+  }
 
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
-
-
-if ($link->query($query) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $link->error;
-}
-
-?>
+  ?>
 </body>
+
 </html>
